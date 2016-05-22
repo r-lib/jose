db3c02c9d84d783a7e0b3f95334fdf16a7c5e92b
Jeroen Ooms <jeroenooms@gmail.com>
2016-05-22 19:59:27 +0200

    Add jwk read/write functions

---
+++ R/jwk_read.R
@@ -0,0 +1,58 @@
+#' @rdname jwk
+#' @param file path to file with key data or literal json string
+#' @export
+jwk_read <- function(file){
+  jwk <- if(is.character(file)){
+    if(jsonlite::validate(file)){
+      jsonlite::fromJSON(file)
+    } else {
+      jsonlite::fromJSON(read_input(file))
+    }
+  } else {
+    file
+  }
+  if(!is.list(jwk) || !length(jwk$kty))
+    stop("File does not have jwk data")
+  key <- switch(jwk$kty,
+    "EC" = jwk_parse_ec(jwk),
+    "RSA" = jwk_parse_rsa(jwk),
+    stop("Unknown key type: ", jwk$kty)
+  )
+  pubkey <- if(inherits(key, "key")){
+    openssl:::derive_pubkey(key)
+  } else {
+    key
+  }
+  type <- openssl:::pubkey_type(pubkey)
+  structure(key, class = c(class(key), type))
+}
+
+jwk_parse_ec <- function(input){
+  curve <- toupper(input$crv)
+  x <- base64url_decode(input$x)
+  y <- base64url_decode(input$y)
+  if(length(input$d)){
+    d <- base64url_decode(input$d)
+    key <- openssl:::ecdsa_key_build(x, y, d, curve)
+    structure(key, class = "key")
+  } else {
+    pubkey <- openssl:::ecdsa_pubkey_build(x, y, curve)
+    structure(pubkey, class = "pubkey")
+  }
+}
+
+#' @importFrom openssl bignum
+jwk_parse_rsa <- function(input){
+  e <- bignum(base64url_decode(input$e))
+  n <- bignum(base64url_decode(input$n))
+  if(length(input$d)){
+    p <- bignum(base64url_decode(input$p))
+    q <- bignum(base64url_decode(input$q))
+    d <- bignum(base64url_decode(input$d))
+    key <- openssl:::rsa_key_build(e, n, p, q, d)
+    structure(key, class = "key")
+  } else {
+    pubkey <- openssl:::rsa_pubkey_build(e, n)
+    structure(pubkey, class = "pubkey")
+  }
+}
---
+++ R/jwk_write.R
@@ -0,0 +1,47 @@
+#' JSON web-keys
+#'
+#' Read and write RSA, ECDSA or AES keys as JSON web keys.
+#'
+#' @export
+#' @rdname jwk
+#' @name jwk
+#' @examples # generate an ecdsa key
+#' library(openssl)
+#' key <- ec_keygen("P-521")
+#' jwk_write(key)
+#' jwk_write(as.list(key)$pubkey)
+#'
+#' # Same for RSA
+#' key <- rsa_keygen()
+#' jwk_write(key)
+#' jwk_write(as.list(key)$pubkey)
+jwk_write <- function(x, path = NULL){
+  str <- jwk_export(x)
+  if(is.null(path)) return(str)
+  writeLines(str, path)
+  invisible(path)
+}
+
+jwk_export <- function(x, ...){
+  UseMethod("jwk_export")
+}
+
+jwk_export.ecdsa <- function(x, ...){
+  keydata <- as.list(x)$data
+  out <- list (
+    kty = "EC",
+    crv = keydata$curve,
+    x = base64url_encode(keydata$x),
+    y = base64url_encode(keydata$y)
+  )
+  if(length(keydata$secret))
+    out$d <- base64url_encode(keydata$secret)
+  to_json(out)
+}
+
+jwk_export.rsa <- function(x, ...){
+  keydata <- as.list(x)$data
+  out <- lapply(keydata, base64url_encode)
+  out$kty <- "RSA"
+  to_json(out)
+}
