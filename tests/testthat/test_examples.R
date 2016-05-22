3d270134928a0cf87ecd961c3cc37e141f2118da
Jeroen Ooms <jeroenooms@gmail.com>
2016-05-22 20:11:27 +0200

    Error message

--- tests/testthat/test_examples.R
+++ tests/testthat/test_examples.R
@@ -15,5 +15,5 @@ MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDdlatRjRjogo3WojgGHFHYLugdUWAY9iR3fy4arWNA
   sig <- "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.EkN-DOsnsuRjRO6BxXemmJDm3HbxrbRzXglbN2S4sOkopdU4IsDxTI8jO19W_A4K8ZPJijNLis4EZsHeY559a4DFOd50_OqgHGuERTqYZyuhtF39yxJPAjUESwxk2J5k_4zM3O-vtd1Ghyo4IbqKKSy6J9mTniYJPenn5-HIirE"
   pk <- openssl::read_pubkey(pubkeystring)
   expect_equal(jwt_decode_rsa(sig, pk)$sub, "1234567890")
-  expect_error(jwt_decode_rsa(sig, openssl::rsa_keygen())$sub, "verif")
+  expect_error(jwt_decode_rsa(sig, openssl::rsa_keygen())$sub, "fail")
 })
