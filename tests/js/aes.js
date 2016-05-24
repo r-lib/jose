0090ee965b319504ba466dcc74549ec6470a2bf3
Jeroen Ooms <jeroenooms@gmail.com>
2016-05-25 00:40:00 +0200

    AES unit tests

--- tests/js/aes.js
+++ tests/js/aes.js
@@ -1,3 +1,11 @@
+function str2buf(str) {
+  var bufView = new Uint8Array(str.length);
+  for (var i=0, strLen=str.length; i<strLen; i++) {
+    bufView[i] = str.charCodeAt(i);
+  }
+  return bufView;
+}
+
 algos = ["AES-CTR", "AES-CBC", "AES-GCM"];
 algos.forEach(function(algo){
 	window.crypto.subtle.generateKey({
@@ -7,5 +15,14 @@ algos.forEach(function(algo){
 		window.crypto.subtle.exportKey("jwk", key).then(function(keydata){
 			console.log(JSON.stringify(keydata));
 		});
+    window.crypto.subtle.encrypt({
+      name: algo,
+      iv: new Uint8Array(16),
+      counter: new Uint8Array(16),
+      length: 128, //can be 1-128
+    },key, str2buf("testje")).then(function(encrypted){
+      var base64String = btoa(String.fromCharCode.apply(null, new Uint8Array(encrypted)));
+      console.log("bits: " + base64String);
+    });
 	});
 });
---
+++ tests/testthat/test_aes.R
@@ -0,0 +1,21 @@
+context("AES encryption")
+
+# Note: all tests use IV/Counter 0
+
+test_that("AES algos", {
+  secret <- charToRaw("testje")
+
+  aes_cbc_key <- jwk_read("../keys/aes_cbc.json")
+  aes_cbc_msg <- readBin("../keys/aes_cbc.bin", raw(), 1000)
+  expect_identical(openssl::aes_cbc_decrypt(aes_cbc_msg, key = aes_cbc_key), secret)
+
+  aes_ctr_key <- jwk_read("../keys/aes_ctr.json")
+  aes_ctr_msg <- readBin("../keys/aes_ctr.bin", raw(), 1000)
+  expect_identical(openssl::aes_ctr_decrypt(aes_ctr_msg, key = aes_ctr_key), secret)
+
+  aes_gcm_key <- jwk_read("../keys/aes_gcm.json")
+  aes_gcm_msg <- readBin("../keys/aes_gcm.bin", raw(), 1000)
+
+  #doesn't work, msg too long? Padding?
+  #expect_identical(aes_gcm_decrypt(aes_gcm_msg, key = aes_gcm_key), secret)
+})
