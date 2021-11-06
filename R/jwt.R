04dd9aca65d8a20aeeae82ec47fd32c5be49e42c
Jan <dix.jan@gmx.de>
2021-11-06 14:04:37 +0100

    fix #8 (#9)

--- R/jwt.R
+++ R/jwt.R
@@ -154,7 +154,7 @@ jwt_split <- function(jwt){
     stop("Invalid algorithm: ", header$alg)
   keysize <- as.numeric(substring(header$alg, 3))
   type <- match.arg(substring(header$alg, 1, 1), c("HMAC", "RSA", "ECDSA"))
-  list(type = type, keysize = keysize, data = data, sig = sig, payload = payload)
+  list(type = type, keysize = keysize, data = data, sig = sig, payload = payload, header = header)
 }
 
 to_json <- function(x){
